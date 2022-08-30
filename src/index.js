094449c434d28ac4e0c118286a0cbb6919db3555
Mario Quezada Galan <77445489+moseqmoseg@users.noreply.github.com>
2022-08-30 21:30:14 +0200

    Added some changes

--- src/index.js
+++ src/index.js
@@ -12,34 +12,17 @@ const colors = [
   "var(--baby-powder)",
 ];
 
-// function changeText() {
-//   fetch("https://www.boredapi.com/api/activity/")
-//     .then((response) => response.json())
-//     .catch((error) => console.log(`this shit didnt work because ${error}`))
-//     .then((data) => {
-//       activityBox.innerHTML = data.activity;
-//       button.innerHTML= "Na, hit me again"
+function changeText() {
+  fetch("http://www.boredapi.com/api/activity/")
+    .then((response) => response.json())
+    .catch((error) => console.log(`this shit didnt work because ${error}`))
+    .then((data) => {
+      activityBox.innerHTML = data.activity;
+      button.innerHTML = "Na, hit me again";
 
-//       activityBox.style.color =
-//         colors[Math.floor(Math.random() * colors.length)];
-//         button.style.color=colors[Math.floor(Math.random() * colors.length)];
-//     });
-// }
-
-async function changeText() {
-  try {
-    const response = await fetch("https://whattudooserver.netlify.app/");
-    if (!response.ok) {
-      throw new Error(`HTTP error: ${response.status}`);
-    }
-
-    const data = await response.json();
-    activityBox.innerHTML = data.activity;
-    button.innerHTML = "Na, hit me again";
-
-    activityBox.style.color = colors[Math.floor(Math.random() * colors.length)];
-    button.style.color = colors[Math.floor(Math.random() * colors.length)];
-  } catch (error) {
-    console.error(`Could not get activity: ${error}`);
-  }
+      activityBox.style.color =
+        colors[Math.floor(Math.random() * colors.length)];
+      button.style.color = colors[Math.floor(Math.random() * colors.length)];
+    });
 }
+
